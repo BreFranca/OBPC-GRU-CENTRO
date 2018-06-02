dbeda85813704c025de847f23772ca0ed1549c3f
Breno Franca <bre.sfranca@gmail.com>
2018-06-02 18:05:42 -0300

    Initial Commit

---
+++ html/assets/js/script.js
@@ -0,0 +1,29 @@
+jQuery(function($){
+   $("#tel").mask("(99) 99999-9999?");
+	$('.slider').slick({
+	  dots: false,
+	  arrows: false,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 3,
+	  slidesToScroll: 3,
+	  responsive: [	
+	    {
+	      breakpoint: 769,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1,
+	        arrows: true,
+	        dots: true
+	      }
+	    }
+	    // You can unslick at a given breakpoint now by adding:
+	    // settings: "unslick"
+	    // instead of a settings object
+	  ]
+	});
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+	});
+});
